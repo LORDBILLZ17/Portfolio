c54b24323fe2a1ffff1cabbc57dae88a3b4889d3
LORDBILLZ17 <bilalmujeeb2008@gmail.com>
2025-10-16 13:41:11 +0100

    updates

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -61,10 +61,10 @@ const Hero = () => {
     
     // Path to your resume file in the public folder
     // Make sure to place your resume.pdf file in the public folder
-    link.href = 'public/Alaaya_Bilal_Resume (1).docx';
+    link.href = '/public/Alaaya_Bilal_Resume (1).docx';
     
     // Set the download attribute with the desired file name
-    link.download = 'public/Alaaya_Bilal_Resume (1).docx';
+    link.download = 'My Resume';
     
     // Append to body, click, and remove
     document.body.appendChild(link);
